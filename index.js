10c2fa4bbe84a0979c179874416570f1e5601359
Tyler Hall <thall2980@gmail.com>
2022-08-30 14:29:14 -0400

    commit tree ab184c56a7ee2deb47b6e37ef56eddebfbfe93cc

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(activity) {
+    activity = activity || 'roller-skate'
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+    
+const mondayWork = function (mondayActivity){
+    mondayActivity = mondayActivity || 'go to the office'
+    return `This Monday, I will ${mondayActivity}.`;
+}
+
+
+
+function wrapAdjective (party = '*') {
+    return function (adjective = "special") {
+    return `You are ${party}${adjective}${party}!`
+    }
+}
